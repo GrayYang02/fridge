a0da1b3028705ff765db786c8d575451063555f7
Hancheng Yang <hanchengyang@Hanchengs-MacBook-Air.local>
2025-02-20 15:40:29 -0500

    update a default model to django

---
+++ backend/demo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Food(models.Model):
+    Name = models.CharField(blank=True, unique=True, max_length=36)
+    Production_Date = models.DateField(blank=True, null=True)
+    Expire_Date = models.DateField(blank=True, null=True)
+    # def __str__(self):
+    #     return self.name
