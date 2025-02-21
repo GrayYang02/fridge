96aba244ac6ecb8ccd09117ebc5446c3a8caa674
Hancheng Yang <hanchengyang@eduroam-campus-10-36-205-135.campus-dynamic.uwaterloo.ca>
2025-02-21 16:04:31 -0500

    update views

--- backend/demo/urls.py
+++ backend/demo/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
+from .views import Another
 
 urlpatterns = [
-    path('demo/',include('demo.urls')),
-    path('admin/', admin.site.urls),
+    path('first', views.first),
+    path('another', Another.as_view())
 ]
--- backend/demo/views.py
+++ backend/demo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View
+from .models import Food
 # Create your views here.
+
+class Another(View):
+
+    foods = Food.objects.all()
+    output = ''
+    for food in foods:
+        output += f"We have {food.Name} food with ID {food.id}<br>"
+
+    def get(self, request):
+        return HttpResponse(self.output)
+
+def first(request):
+    return HttpResponse('First message from views')
