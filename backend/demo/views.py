b9869e21ae211b661590aa8dda0bb698a13f9e82
Nicole <1721821109@qq.com>
2025-02-23 17:35:11 -0500

    m

--- backend/demo/views.py
+++ backend/demo/views.py
@@ -1,8 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Food, User, Recipe, UserRecipeLog, FridgeItem
 from .serializers import FoodSerializer, UserSerializer, RecipeSerializer, UserRecipeLogSerializer, FridgeItemSerializer
 
 class FoodViewSet(ModelViewSet):
+    def create(self, request):
+
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
 
