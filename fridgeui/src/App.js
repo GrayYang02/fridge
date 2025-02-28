79006751e156f85297aea725fa9ffb6d488d1198
Daxin-H <huangd5@rpi.edu>
2025-02-28 17:55:59 -0500

    feat: recipe generate

--- fridgeui/src/App.js
+++ fridgeui/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
- 
+
 import { BrowserRouter as Router, Routes, Route, Link, Navigate, BrowserRouter } from 'react-router-dom';
- 
+
 import SignUp from './components/SignupLogin/SignUp';
 import Profile from './components/Profile/Profile';
 import Login from './components/SignupLogin/Login';
 import ProtectedRoute from './components/ProtectedRoute';
 import NotFound from './components/NotFound/NotFound';
 import Fridge from "./components/Fridge/Fridge";
- 
-import Recipe from './components/Recipe/Recipe'
- 
+
+import RecipeGen from './components/RecipeGenerate/RecipeGen'
+
 
 function Logout() {
   localStorage.clear();
@@ -32,11 +32,11 @@ function App() {
         <Route path="/signup" element={<RegisterAndLogout />} />
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
- 
+
         <Route path="/profile" element={<ProtectedRoute><Profile></Profile></ProtectedRoute>} />
         <Route path="/fridge" element={<Fridge />} />
-        <Route path="/recipe" element={<Recipe />} />
- 
+        <Route path="/recipe_gen" element={<RecipeGen />} />
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
