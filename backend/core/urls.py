b3107a6acc079bfc87f1b49cdaa7bedb596d7f4b
Hancheng Yang <hanchengyang@Hanchengs-MacBook-Air.local>
2025-03-01 18:25:51 -0500

    write unit tests for backend views

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from core.views import FridgeItemViewSet 
 from .api_chat import get_recipe
 from .views import  UserViewSet, RecipeViewSet, UserRecipeLogViewSet, FridgeItemViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -13,12 +13,10 @@ from .views import (
     LoginView
 )
 
-
 router = DefaultRouter()
 router.register(r'users', UserViewSet)  # `/users/`
 router.register(r'recipes', RecipeViewSet)  # `/recipes/`
 router.register(r'user-recipe-log', UserRecipeLogViewSet)  # `/user-recipe-log/`
-# router.register(r'fridge', FridgeItemViewSet)  # `/fridge/`
 router.register(r'fridge', FridgeItemViewSet, basename="fridge") 
 urlpatterns = [
     path('', include(router.urls)),
@@ -28,6 +26,4 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api_auth/', include('rest_framework.urls')),
     path('get_recipe/', get_recipe, name='get_recipe'),  # POST request will call this function
-
-
 ]
