1bed58ba08e023599d75d140bd4f30bc6fb1c598
Yixuan Chen <yixuanchen@Yixuans-MacBook-Pro-2.local>
2025-03-04 15:35:07 -0500

    mod settings.py token time

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,8 +25,8 @@ REST_FRAMEWORK = {
 
 }
 SIMPlE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    # 'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 
 }
